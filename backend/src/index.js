6aff176612ccb057248e72727e7516aa7d8eb052
FelipeMartinsFrudeli <valfrudeli@gmail.com>
2020-03-25 18:31:17 -0300

    Front-end do app Completo!

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 const porta = 3003;
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
